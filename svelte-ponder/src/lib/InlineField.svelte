5142c7e207a31b2879378d522c896911120539e5
ButterMyLoaf23 <bostonwyatt1@gmail.com>
2026-05-30 21:05:44 -0700

    Inlinefield.svelte component completed

---
+++ svelte-ponder/src/lib/InlineField.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+
+    let {
+        label,
+        value = $bindable(),
+        type = "text"
+    } = $props< {
+        label: String;
+        value: String;
+        type?: "text" | "email" | "textarea";
+    } >();
+
+    let isEditing = $state(false);
+    let originalValue = value;
+
+    function startEdit() {
+        originalValue = value;
+        isEditing = true;
+    }
+
+    function commitEdit() {
+        isEditing = false;
+    }
+
+    function cancelEdit() {
+        value = originalValue;
+        isEditing = false;
+    }
+
+    function onInputKeyDown(event: KeyboardEvent) {
+        if (event.key === "Enter" && type !== "textarea") {
+            event.preventDefault();
+            commitEdit();
+        }
+
+        if (event.key === "Escape") {
+            event.preventDefault();
+            commitEdit();
+        }
+    }
+    
+</script>
+
+<div class="field">
+    <div class="label">{label}</div>
+    {#if isEditing}
+        {#if type === "textarea"}
+            <textarea
+            bind:value
+            onblur = {commitEdit}
+            onkeydown = {onInputKeyDown}>
+            </textarea>
+        {:else}
+            <input
+                {type}
+                bind:value
+                onblur = {commitEdit}
+                onkeydown = {onInputKeyDown} />
+        {/if}
+    {:else}
+        <p
+            class = "value"
+            onclick = {startEdit}>{value}</p>
+    {/if}
+</div>
+
+<style></style>
+
